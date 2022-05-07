2ca9d66d73f2b1a2a30097ea9d99ec63c5cf29c2
jayson7 <lexxiijoo70@gmail.com>
2022-05-07 06:46:27 +0100

    create task

--- frontend/src/Components/ModifyTask/createTask.jsx
+++ frontend/src/Components/ModifyTask/createTask.jsx
@@ -21,11 +21,7 @@ function CreateTask() {
         </Modal.Header>
         <Modal.Body>
           <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </p>
+          <p></p>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
